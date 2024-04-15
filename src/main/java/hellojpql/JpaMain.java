86c5e44e052a3d6b6b369877ab26e0faf829549e
MelRanG <tpdud285@gmail.com>
2024-04-15 20:45:32 +0900

    JPQL기본

--- src/main/java/hellojpql/JpaMain.java
+++ src/main/java/hellojpql/JpaMain.java
@@ -1,10 +1,9 @@
 package hellojpql;
 
 import hellojpql.jpql.Member;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,6 +15,13 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             em.persist(member);
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> queryString = em.createQuery("select m.username from Member m", String.class);
+            Query query2 = em.createQuery("select m.username, m.age from Member m");
+
+            List<Member> resultList = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getResultList();
             tx.commit();
         }catch (Exception e){
             tx.rollback();
