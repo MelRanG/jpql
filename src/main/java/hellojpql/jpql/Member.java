899468baa7486d2bd7b84d0173b41f8265c180be
MelRanG <tpdud285@gmail.com>
2024-04-15 20:09:12 +0900

    first commit

---
+++ src/main/java/hellojpql/jpql/Member.java
@@ -0,0 +1,18 @@
+package hellojpql.jpql;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+}
